65c7f307c7bfb6c71713231fd10695d3ae9eb3d3
KotoFeu3434 <142950180+KotoFeu3434@users.noreply.github.com>
2025-10-07 23:36:52 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ClickGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelScore = new System.Windows.Forms.Label();
+            this.labelLast = new System.Windows.Forms.Label();
+            this.labelHigh = new System.Windows.Forms.Label();
+            this.buttonClick = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Location = new System.Drawing.Point(25, 20);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(43, 16);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "час: 0";
+            // 
+            // labelScore
+            // 
+            this.labelScore.AutoSize = true;
+            this.labelScore.Location = new System.Drawing.Point(173, 83);
+            this.labelScore.Name = "labelScore";
+            this.labelScore.Size = new System.Drawing.Size(73, 16);
+            this.labelScore.TabIndex = 1;
+            this.labelScore.Text = "рахунок: 0";
+            // 
+            // labelLast
+            // 
+            this.labelLast.AutoSize = true;
+            this.labelLast.Location = new System.Drawing.Point(271, 314);
+            this.labelLast.Name = "labelLast";
+            this.labelLast.Size = new System.Drawing.Size(132, 16);
+            this.labelLast.TabIndex = 2;
+            this.labelLast.Text = "останнія рахунок: 0";
+            // 
+            // labelHigh
+            // 
+            this.labelHigh.AutoSize = true;
+            this.labelHigh.Location = new System.Drawing.Point(12, 314);
+            this.labelHigh.Name = "labelHigh";
+            this.labelHigh.Size = new System.Drawing.Size(70, 16);
+            this.labelHigh.TabIndex = 3;
+            this.labelHigh.Text = "рекорд:  0";
+            // 
+            // buttonClick
+            // 
+            this.buttonClick.Location = new System.Drawing.Point(158, 166);
+            this.buttonClick.Name = "buttonClick";
+            this.buttonClick.Size = new System.Drawing.Size(110, 79);
+            this.buttonClick.TabIndex = 4;
+            this.buttonClick.Text = "Клікни";
+            this.buttonClick.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 356);
+            this.Controls.Add(this.buttonClick);
+            this.Controls.Add(this.labelHigh);
+            this.Controls.Add(this.labelLast);
+            this.Controls.Add(this.labelScore);
+            this.Controls.Add(this.labelTime);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelScore;
+        private System.Windows.Forms.Label labelLast;
+        private System.Windows.Forms.Label labelHigh;
+        private System.Windows.Forms.Button buttonClick;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Windows.Forms;
+
+namespace ClickGame
+{
+    public partial class Form1 : Form
+    {
+        int counter = 0;      
+        int timeLeft = 30;      
+        int lastScore = 0;    
+        int highScore = 0;      
+        bool gameStarted = false;
+        Timer countdownTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            buttonClick.Click += buttonClick_Click;
+
+            MessageBox.Show("Натисніть ПРОБІЛ, щоб почати гру!");
+
+            countdownTimer = new Timer();
+            countdownTimer.Interval = 1000;
+            countdownTimer.Tick += CountdownTimer_Tick;
+
+        
+            this.KeyPreview = true;
+            this.KeyDown += Form1_KeyDown;
+
+            
+            if (File.Exists("LastScore.json"))
+            {
+                string json = File.ReadAllText("LastScore.json");
+                lastScore = JsonSerializer.Deserialize<int>(json);
+                labelLast.Text = $"Останній рахунок: {lastScore}";
+            }
+
+   
+            if (File.Exists("HighScore.json"))
+            {
+                string json = File.ReadAllText("HighScore.json");
+                highScore = JsonSerializer.Deserialize<int>(json);
+                labelHigh.Text = $"Рекорд: {highScore}";
+            }
+
+           
+            labelTime.Text = $"Час: {timeLeft}";
+            labelScore.Text = $"Рахунок: {counter}";
+        }
+         
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+           
+
+            if (e.KeyCode == Keys.Space && !gameStarted)
+            {
+                gameStarted = true;
+                counter = 0;
+                timeLeft = 30;
+
+                labelTime.Text = $"Час: {timeLeft}";
+                labelScore.Text = $"Рахунок: {counter}";
+
+                countdownTimer.Start();
+                MessageBox.Show("Гра почалася!");
+            }
+        }
+
+        private void CountdownTimer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft--;
+                labelTime.Text = $"Час: {timeLeft}";
+            }
+            else
+            {
+                countdownTimer.Stop();
+                gameStarted = false;
+
+                lastScore = counter;
+                labelLast.Text = $"Останній рахунок: {lastScore}";
+
+         
+                if (counter > highScore)
+                {
+                    highScore = counter;
+                    labelHigh.Text = $"Рекорд: {highScore}";
+                    File.WriteAllText("HighScore.json",
+                        JsonSerializer.Serialize(highScore, new JsonSerializerOptions { WriteIndented = true }));
+                    MessageBox.Show($"Новий рекорд! {highScore} ");
+                }
+
+         
+                File.WriteAllText("LastScore.json",
+                    JsonSerializer.Serialize(lastScore, new JsonSerializerOptions { WriteIndented = true }));
+
+                MessageBox.Show($"Гру завершено! Ваш рахунок: {counter}");
+            }
+        }
+
+        private void buttonClick_Click(object sender, EventArgs e)
+        {
+            if (gameStarted)
+            {
+                counter++;
+                labelScore.Text = $"Рахунок: {counter}";
+            }
+            else
+            {
+                MessageBox.Show("Спочатку натисніть ПРОБІЛ, щоб почати гру!");
+            }
+        }
+    }
+}
